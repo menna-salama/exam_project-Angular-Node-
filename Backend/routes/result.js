253008e2f5c45caec679f1b8320d97040168b6a4
Menna Salama <mennasalama196@gmail.com>
2025-06-16 22:46:59 +0300

    Delete Backend directory

--- Backend/routes/result.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { auth } = require("../middlewares/auth");
-const resultController = require('../controllers/result');
-router.get('/admin/results',auth, resultController.getAllResults);
-
-module.exports = router;
